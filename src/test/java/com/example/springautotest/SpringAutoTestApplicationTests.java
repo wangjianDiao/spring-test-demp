9233d8c5e8bcf09a9ea97191c57c4e26cb90d044
wangjianDiao <756862057@qq.com>
2023-09-15 21:33:12 +0800

    add by wangjian

---
+++ src/test/java/com/example/springautotest/SpringAutoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springautotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAutoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
