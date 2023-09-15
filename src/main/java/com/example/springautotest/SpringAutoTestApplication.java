62e3e5d1ddc34fd9b37615e4b51880bb42ec71f2
wangjianDiao <756862057@qq.com>
2023-09-15 21:29:13 +0800

    add by wangjian

---
+++ src/main/java/com/example/springautotest/SpringAutoTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springautotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAutoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAutoTestApplication.class, args);
+    }
+
+}
